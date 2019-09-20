13873e24a1035e4d2d5edfb2ff01d04f297e512e
Tobie Tsuzuki <tobie.tsuzuki@gmail.com>
2019-09-20 16:45:37 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,12 +60,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
-# }
-
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -81,6 +75,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
+     
 programmer_hash[:yukihiro_matsumoto] = {
     :known_for => "Ruby",
     :languages => ["LISP", "C"]
